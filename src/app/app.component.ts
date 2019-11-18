2d1a1665ac7e93336cefe107479ad76b81e2c8f1
aleami <alessia.amitranobo@gmail.com>
2019-11-18 17:45:31 +0000

    looking better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'ngdemodirect';
+  title = "ngdemodirect";
+  counter = 0;
+
+  increment(event: any) {
+    console.log("it works");
+    ++this.counter;
+  }
 }
